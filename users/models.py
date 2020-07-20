be7b2816b12b9b3df9c38601f8204184e709f5f3
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-07-21 03:56:26 +0400

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField(default="default.jpg", upload_to='user_images')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Ник')
+    img = models.ImageField(default="default.jpg", upload_to='user_images', verbose_name='Загрузить картинку')
 
     def __str__(self):
-        return f'Установить аватарку {self.user.username}'
+        return f'Установить аву {self.user.username}'
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         super().save(force_insert, force_update, using, update_fields)

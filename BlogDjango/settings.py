7c44526bcb074c542fa07660e86a4dd307a46ee1
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-07-21 17:37:49 +0400

    update backend

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -128,12 +128,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cheferock132@gmail.com'
-EMAIL_HOST_PASSWORD = 'sfsef3wg'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'cheferock132@gmail.com'
+# EMAIL_HOST_PASSWORD = 'sfsef3wg'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
